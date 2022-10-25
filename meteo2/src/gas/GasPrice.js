1fcacafe489ab492e385cadd57b98298606e0549
Maxence Aïci <maxence.aici@alan.eu>
2022-10-25 08:34:22 +0200

    dynamic gas price

--- meteo2/src/gas/GasPrice.js
+++ meteo2/src/gas/GasPrice.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import { getGasPrice } from "./getGasPrice";
 
 function GasPrice() {
   const gasPrice = getGasPrice();
-  return <h1>Le prix de l'essence est {gasPrice} €</h1>;
+  const gasPriceInDollars = gasPrice * 1.2;
+  const [showInDollars, setShowInDollars] = useState(false);
+
+  return (
+    <div>
+      {showInDollars ? (
+        <h1>Le prix de l'essence est {gasPriceInDollars} $</h1>
+      ) : (
+        <h1>Le prix de l'essence est {gasPrice} €</h1>
+      )}
+
+      <label htmlFor="toggle">Show price in dollars</label>
+      <input
+        id="toggle"
+        type="checkbox"
+        onChange={(e) => setShowInDollars(e.target.checked)}
+        checked={showInDollars}
+      />
+    </div>
+  );
 }
 
 export default GasPrice;
--- meteo2/src/gas/GasPrice.test.js
+++ meteo2/src/gas/GasPrice.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import GasPrice from "./GasPrice";
 import { getGasPrice } from "./getGasPrice";
 
@@ -18,5 +18,18 @@ test("renders another gas price message", () => {
   render(<GasPrice />);
   expect(
     screen.getByText("Le prix de l'essence est 1.5 €"),
+  ).toBeInTheDocument();
+});
+
+test("show the gas price in dollar when asked to", () => {
+  getGasPrice.mockReturnValue(1);
+
+  render(<GasPrice />);
+  expect(screen.getByText("Le prix de l'essence est 1 €")).toBeInTheDocument();
+
+  fireEvent.click(screen.getByLabelText(/show/i));
+
+  expect(
+    screen.getByText("Le prix de l'essence est 1.2 $"),
   ).toBeInTheDocument();
 });
